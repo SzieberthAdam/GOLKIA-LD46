d06b0f5f83f4902c95c150cb41657e9ac4e4db5f
SzieberthAdam <sziebadam@gmail.com>
2020-04-19 03:57:23 +0200

    gol shader

--- src/scene/game.lua
+++ src/scene/game.lua
@@ -7,52 +7,94 @@ M.init = function()
   M.turnframes = 60
   M.setrandom()
   M.relaxframes = M.turnframes
-  M.update_canvas()
 
   M.golcanvas = love.graphics.newCanvas(
     conf.worldcols, conf.worldrows
     --conf.screen_width, conf.screen_height
   )
+  M.golcanvas1 = love.graphics.newCanvas(
+    conf.worldcols, conf.worldrows
+  )
+  M.update_canvas()
 
   local pixelcode = [[
-    vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
+
+    vec2 offset_coords(vec2 texture_coords, vec2 offset) {
+      float x = texture_coords[0] * love_ScreenSize.x;
+      float y = texture_coords[1] * love_ScreenSize.y;
+      x += offset[0];
+      y += offset[1];
+      if (love_ScreenSize.x < x) {
+        x -= love_ScreenSize.x;
+      }
+      else if (x < 0) {
+        x += love_ScreenSize.x;
+      }
+      if (love_ScreenSize.y < y) {
+        y -= love_ScreenSize.y;
+      }
+      else if (y < 0) {
+        y += love_ScreenSize.y;
+      }
+      x /= love_ScreenSize.x;
+      y /= love_ScreenSize.y;
+      return vec2(x, y);
+    }
+
+    vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
     {
-        vec4 texcolor = Texel(tex, texture_coords);
-        return texcolor * color;
+      vec4 newcolor;
+      vec4 texcolor = Texel(tex, texture_coords);
+      int sum =
+          int(Texel(tex, offset_coords(texture_coords, vec2(-1.0, -1.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2(-1.0,  0.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2(-1.0,  1.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2( 0.0, -1.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2( 0.0,  1.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2( 1.0, -1.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2( 1.0,  0.0))).r)
+        + int(Texel(tex, offset_coords(texture_coords, vec2( 1.0,  1.0))).r)
+      ;
+      if (sum == 3) {
+        newcolor = vec4(1.0, 1.0, 1.0, 1.0);
+      } else if (sum == 2) {
+        newcolor = texcolor;
+      } else {
+        newcolor = vec4(0.0, 0.0, 0.0, 1.0);
+      }
+      //newcolor = Texel(tex, vec2(texture_coords[0]+1/love_ScreenSize.x,texture_coords[1]+1/love_ScreenSize.y));//
+      //newcolor = Texel(tex, offset_coords(texture_coords, vec2( 0.0,  1.0)));
+      return newcolor;
     }
   ]]
-  M.golshader = love.graphics.newShader(pixelcode, vertexcode)
-
+  M.golshader = love.graphics.newShader(pixelcode)
+  M.withshader = false
 end
 
 M.draw = function()
-  if M.golcanvas
-  then
-    love.graphics.clear(0, 0, 0, 0)
-    love.graphics.draw(M.golcanvas, 0, 0, 0, conf.tilew, conf.tilew)
-  end
-  love.graphics.print("hello",10,10)
+  love.graphics.clear(0, 0, 0, 0)
+  love.graphics.draw(M.golcanvas1, 0, 0, 0, conf.tilew, conf.tilew)
 end
 
 M.update = function()
   M.relaxframes = M.relaxframes - 1
   if M.relaxframes == 0
   then
-    M.setrandom()
+    print(M.withshader)
     M.relaxframes = M.turnframes
-    M.update_canvas()
+    M.update_canvas(M.withshader)
+    M.withshader = not M.withshader
   end
 end
 
-M.update_canvas = function()
+M.update_canvas = function(withshader)
   love.graphics.setCanvas(M.golcanvas)
-  love.graphics.setShader(shader)
   love.graphics.clear(0, 0, 0, 0)
   for r, worldrow in ipairs(M.world)
   do
     for c, v in ipairs(worldrow)
     do
-      love.graphics.setColor(0.3*v, 0.6*v, v, 1)  -- important to reset
+      love.graphics.setColor(v, v, v, 1)  -- important to reset
       love.graphics.rectangle(
         "fill",
         --(c-1)*conf.tilew,
@@ -67,7 +109,12 @@ M.update_canvas = function()
     end
   end
   love.graphics.setColor(1,1,1,1) -- important reset
-  love.graphics.setShader()
+  love.graphics.setCanvas()
+  love.graphics.setCanvas(M.golcanvas1)
+  if M.withshader then love.graphics.setShader(M.golshader) end
+  love.graphics.clear(0, 0, 0, 0)
+  love.graphics.draw(M.golcanvas)
+  if M.withshader then love.graphics.setShader() end
   love.graphics.setCanvas()
 end
 
